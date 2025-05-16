d4998cd71b3ab5a1c4d70f495b3bfbc4295f6944
Great-Software-Company <jouni@greatsoftwarecompany.com>
2025-05-16 17:44:53 +0300

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,416 @@
+const exampleArray = ["Apple", "Banana", "Grapefruit", "Orange", "Durian", "Pear", "Yellow plum", "Açaí"];
+const sqlTblName = "test_tbl";
+const sqlColName = "test_column";
+
+$(document).ready(function () {
+
+
+  $(".selectBox").on("click", function (e) {
+    $(this).addClass("show");
+    var dropdownItem = e.target;
+    var container = $(this).find(".selectBox__value");
+    container.text(dropdownItem.text);
+    $(dropdownItem).addClass("active").siblings().removeClass("active");
+  });
+  $("#transformDropdown .dropdown-menu>div").click(function (e) {
+    e.stopPropagation();
+  });
+  $("#transformDropdown .dropdown-toggle").click(function () {
+    let outputDataFormat = $("#outputDataFormat").text();
+    if (outputDataFormat.trim().toLowerCase() === "sql insert") $("#maxrowwidthbox").hide();
+    else $("#maxrowwidthbox").show();
+  });
+  let inputDataEle = $("#inputData");
+  $("#exampleBtn").click(function () {
+    let inputDataFormat = $("#inputDataFormat").text();
+
+    switch (inputDataFormat.trim()) {
+      case "Comma separated":
+        inputDataEle.val(exampleArray.toString());
+        break;
+      case "Tab separated":
+        inputDataEle.val(exampleArray.join("\t"));
+        break;
+      case "One item per row":
+        inputDataEle.val(exampleArray.join("\n"));
+        break;
+      case "Inside quotation marks":
+        let temp = "Some tasty fruits include: ";
+        sep = ", ";
+        for (let i = 0; i < exampleArray.length; i++) {
+          if (i === exampleArray.length - 2) sep = " and ";
+          if (i === exampleArray.length - 1) sep = "";
+          temp += '"' + exampleArray[i] + '"' + sep;
+        }
+        temp += "."
+        inputDataEle.val(temp);
+        break;
+      default:
+        inputDataEle.val(exampleArray.toString());
+    }
+  });
+
+  $("#uploadfile").change(function (e) {
+    $(".loading-element-mask").addClass("active");
+    let inputData = $("#inputData");
+    let file = $(this)[0].files[0];
+    let reader = new FileReader();
+
+    let textFile = /text.*/;
+
+    if (file.type.match(textFile)) {
+      fileLoad(reader);
+    } else {
+      inputData.html(
+        "<span class='error'>It doesn't seem to be a text file!</span>"
+      );
+    }
+    reader.readAsText(file);
+  });
+  $("#outputData").hide();
+
+  let transformClicked = false;
+  let transformTimeout;
+
+
+  $("#convertBtn").click(function () {
+    transformClicked = true;
+    $("#outputData").show();
+    let dataStr = $("#inputData").val();
+    if (dataStr.trim().length == 0) {
+      $("#exampleBtn").click();
+      dataStr = $("#inputData").val();
+    }
+
+    if (dataStr) {
+      if ($("#normalize").is(":checked")) {
+        dataStr = dataStr.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+      }
+      if ($("#lowercase").is(":checked")) {
+        dataStr = dataStr.toLowerCase();
+      }
+      if ($("#htmlEntities").is(":checked")) {
+        dataStr = $("<textarea/>").html(dataStr).html();
+        dataStr = $("<div/>").text(dataStr).html();
+      }
+      $(".loading-element-mask").addClass("active");
+      let inputDataFormat = $("#inputDataFormat").text();
+      let dataArr = [];
+
+      switch (inputDataFormat.trim()) {
+        case "Comma separated":
+          dataArr = dataStr.split(",");
+          outputDataFunc(dataArr);
+          break;
+        case "Tab separated":
+          dataArr = dataStr.split("\t");
+          outputDataFunc(dataArr);
+          break;
+        case "One item per row":
+          dataArr = dataStr.split("\n");
+          outputDataFunc(dataArr);
+          break;
+        case "Inside quotation marks":
+          const re = /(['"])(.*?)\1/g;
+          let current;
+          while (current = re.exec(dataStr)) {
+            dataArr.push(current.pop());
+          }
+          outputDataFunc(dataArr);
+          break;
+        default:
+          inputDataEle.val(data.split(",").toString());
+      }
+    }
+  });
+  function delayedTransform() {
+    if (transformClicked) {
+      clearTimeout(transformTimeout);
+      transformTimeout = setTimeout(() => {
+        $("#convertBtn").click();
+      }, 1000);
+    }
+  }
+  $("#outputDataFormat + .dropdown-menu .dropdown-item").on("click", function() {
+    $("#outputDataFormat  + .dropdown-menu .dropdown-item").removeClass("active");
+    $(this).addClass("active");
+
+    $("#outputDataFormat").text($(this).text());
+
+    delayedTransform();
+  });
+
+  $("#inputDataFormat + .dropdown-menu .dropdown-item").on("click", function() {
+    $("#inputDataFormat + .dropdown-menu .dropdown-item").removeClass("active");
+    $(this).addClass("active");
+
+    $("#inputDataFormat").text($(this).text());
+
+    delayedTransform();
+  });
+  $("#inputDataFormat + .dropdown-menu .dropdown-item").on("click", function() {
+    $("#inputDataFormat + .dropdown-menu .dropdown-item").removeClass("active");
+    $(this).addClass("active");
+
+    $("#inputDataFormat").text($(this).text());
+
+    delayedTransform();
+  });
+  $("#transformDropdown input[type=checkbox]").on("change", delayedTransform);
+
+  $("#copyBtn").click(function (event) {
+    event.preventDefault();
+    let outputData = $("#outputData").text();
+    CopyToClipboard(outputData, "Copied to clipboard!");
+  });
+});
+
+async function fileLoad(reader) {
+  let myPromise = new Promise(function (resolve) {
+    reader.onload = function (event) {
+      resolve(event.target.result);
+    };
+  });
+  let data = await myPromise;
+  let inputDataEle = $("#inputData");
+  let inputDataFormat = $("#inputDataFormat").text();
+
+  setTimeout(() => {
+    switch (inputDataFormat.trim()) {
+      case "Comma separated":
+        inputDataEle.val(data.split(",").toString());
+        break;
+      case "Tab separated":
+        inputDataEle.val(data.split("\t").join("\t"));
+        break;
+      case "One item per row":
+        inputDataEle.val(data.split("\n").join("\n"));
+        break;
+      case "Inside quotation marks":
+        let temp = [];
+        const re = /"([^"]+)"/g;
+        let match;
+        while ((match = re.exec(data)) !== null) {
+          temp.push('"' + match[1] + '"');
+        }
+        inputDataEle.val(temp);
+        break;
+      default:
+        inputDataEle.val(data.split(",").toString());
+    }
+    $(".loading-element-mask").removeClass("active");
+  }, 1000);
+}
+
+function CopyToClipboard(value, notificationText) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $("#outputData").select();
+  document.execCommand("copy");
+  $temp.remove();
+
+  $(".copy-notification").html(
+    `<div class="alert alert-success d-flex mb-0">
+        <div><img width="40px" height="40px" src="./assets/images/icon_message.svg" /></div>
+        <p class="my-auto ml-1">${notificationText}</p>
+      </div>`
+  );
+  alertFunc();
+}
+
+function formatArrayElement(element, quote, outputFormat) {
+  return quote + element + quote;
+}
+
+function formatArrayWithWrapping(elements, options) {
+  const {
+    quote = '"',
+    separator = ", ",
+    outputDataWidth,
+    prefix = "",
+    suffix = ""
+  } = options;
+
+  let outputDataStr = "";
+  let len_sum = 0;
+
+  for (let i = 0; i < elements.length; i++) {
+    const sep = i === elements.length - 1 ? "" : separator;
+    const formattedElement = formatArrayElement(elements[i], quote, options.outputFormat);
+
+    outputDataStr += formattedElement + sep;
+    len_sum += elements[i].length + (quote.length * 2) + sep.length;
+
+    if (len_sum >= outputDataWidth) {
+      outputDataStr += "\n";
+      len_sum = 0;
+    }
+  }
+
+  return prefix + outputDataStr + suffix;
+}
+function unifyWhitespace(str) {
+  // 1. First unescape all Unicode sequences and special characters
+  str = str.replace(/\\u([\dA-F]{4})/gi,
+    (match, grp) => String.fromCharCode(parseInt(grp, 16)))
+    .replace(/\\[tnrfv]/g, (match) =>
+      ({ '\\t': '\t', '\\n': '\n', '\\r': '\r',
+        '\\f': '\f', '\\v': '\v' }[match]));
+
+  // 2. Then handle ALL whitespace and control characters in three ways:
+  return str
+    // A. Regex for known whitespace chars (including Unicode)
+    .replace(/[\s\u0085\u00A0\u1680\u180E\u2000-\u200F\u2028-\u202F\u205F\u3000]/g, ' ')
+
+    // B. Explicit charCode replacement for control chars ≤ 32
+    .split('')
+    .map(c => c.charCodeAt(0) <= 32 ? ' ' : c)
+    .join('')
+
+    // C. Final cleanup of multiple spaces
+    .replace(/\s{2,}/g, ' ')
+    .trim();
+}
+function outputDataFunc(data) {
+  let outputDataFormat = $("#outputDataFormat").text();
+  let outputDataEle = $("#outputData");
+  let outputDataWidth = $("#maxRowWidth").val();
+  let finalOutputDataStr = "";
+  let dataArr = [];
+  if (outputDataWidth < 1) outputDataWidth = 1; // just in case
+
+  data.map((e) => {
+    e = e.replace(/[\n\r]/g, "");
+    if (outputDataFormat.toLowerCase() !== "pascal array" && outputDataFormat.toLowerCase() !== "delphi array") e = e.replace(/(["])/g, "\\$1");
+    else e = e.replace(/(['])/g, "''");
+    if ($("#whitespace").is(":checked")) e = e.trim();
+    if ($("#base64").is(":checked")) e = btoa(e);
+    if ($("#unifyWhitespace").is(":checked")) {
+      e = unifyWhitespace(e);  // Applies all transformations
+    }
+    if ($("#stripWhitespace").is(":checked")) {
+      e = e.replace(/\s{2,}/g, ' '); // Replace 2+ spaces with single space
+    }
+    dataArr.push(e);
+  });
+
+  var sortV = $("#sort").is(":checked");
+  var duplicateV = $("#countDuplicate").is(":checked");
+  if ($("#duplicates").is(":checked")) dataArr = getUnique(dataArr);
+  if (duplicateV) dataArr = countDuplicateElements(dataArr, sortV);
+  if ($("#sort").is(":checked") && !duplicateV) {
+    dataArr.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'}));
+  }
+  if ($("#removeEmptyElement").is(":checked"))
+    dataArr = dataArr.filter(function (el) {
+      return el != "";
+    });
+
+  let outputFormat = outputDataFormat.trim().slice(0, 3).toLowerCase();
+  switch (outputFormat) {
+    case "jav": // JavaScript
+      finalOutputDataStr = formatArrayWithWrapping(dataArr, {
+        quote: '"',
+        outputDataWidth,
+        prefix: "[",
+        suffix: "];"
+      });
+      break;
+
+    case "pas": // Pascal
+      finalOutputDataStr = formatArrayWithWrapping(dataArr, {
+        quote: "'",
+        outputDataWidth,
+        prefix: "Const arr : Array[0.." + (dataArr.length - 1) + "] of String =\n(",
+        suffix: ");"
+      });
+      break;
+
+    case "del": // Delphi
+      finalOutputDataStr = formatArrayWithWrapping(dataArr, {
+        quote: "'",
+        outputDataWidth,
+        prefix: "Const arr : Array of String =\n[",
+        suffix: "];"
+      });
+      break;
+
+    case "php":
+      finalOutputDataStr = formatArrayWithWrapping(dataArr, {
+        quote: '"',
+        outputDataWidth,
+        prefix: "array(",
+        suffix: ");"
+      });
+      break;
+
+    case "pyt": // Python
+      finalOutputDataStr = formatArrayWithWrapping(dataArr, {
+        quote: '"',
+        outputDataWidth,
+        prefix: "[",
+        suffix: "]"
+      });
+      break;
+
+    case "one": // One item per row
+      finalOutputDataStr = dataArr.join("\n") + "\n";
+      break;
+
+    case "sql":
+      finalOutputDataStr = dataArr.map(item =>
+        `INSERT IGNORE INTO ${sqlTblName}(${sqlColName}) VALUES("${item}");`
+      ).join("\n") + "\n";
+      break;
+  }
+
+  setTimeout(() => {
+    outputDataEle.text(finalOutputDataStr);
+    $(".loading-element-mask").removeClass("active");
+  }, 1000);
+}
+
+function getUnique(array) {
+  var uniqueArray = [];
+
+  // Loop through array values
+  for (i = 0; i < array.length; i++) {
+    if (uniqueArray.indexOf(array[i]) === -1) {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
+}
+
+
+function countDuplicateElements(inputArray, sortElements = false) {
+  const counts = {};
+
+  // Count the occurrences of each element in the input array
+  inputArray.forEach((item) => {
+    const trimmedItem = item.trim(); // Trim the element
+    counts[trimmedItem] = (counts[trimmedItem] || 0) + 1;
+  });
+
+  // Create an array of objects with count and element properties
+  const resultArray = Object.entries(counts).map(([element, count]) => ({count, element}));
+
+  // Sort the result array based on counts and element names
+  resultArray.sort((a, b) => {
+    if (sortElements) {
+      // Sort alphabetically by element names
+      const elementA = a.element;
+      const elementB = b.element;
+      return elementA.localeCompare(elementB);
+    } else {
+      // Sort by counts in descending order
+      const countA = parseInt(a.count);
+      const countB = parseInt(b.count);
+      return countB - countA;
+    }
+  });
+
+  // Create an array of strings in the format "count:element"
+  const formattedResult = resultArray.map(({count, element}) => `${count}:${element}`);
+  return formattedResult;
+}
---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+        content="Convert a list of data to array easily with ArrayCat. For example, a CSV file to JavaScript array without duplicate elements.">
+  <meta name="author" content="Macecraft Software">
+  <meta property="og:title" content="ArrayCat">
+  <meta property="og:description"
+        content="Convert a list of data to array easily with ArrayCat. For example, a CSV file to JavaScript array without duplicate elements.">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://arraycat.com/">
+  <meta property="og:image" content="https://arraycat.com/assets/images/arraycat.png">
+  <title>Convert list to array with ArrayCat</title>
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./assets/favicon/favicon-16x16.png"
+  >
+  <link rel="stylesheet" href="./assets/css/bootstrap.css">
+  <link rel="stylesheet" href="./assets/css/loading.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script defer data-domain="arraycat.com" src="https://plausible.io/js/script.js"></script>
+</head>
+<body class="home-page">
+<header>
+  <nav class="navbar navbar-expand-lg mx-auto py-0">
+    <div class="d-flex w-100 justify-content-between align-items-center">
+      <div class="d-flex align-items-center">
+        <div class="navbar-brand mr-2">
+          <img
+            width="90"
+            class="img-fluid"
+            src="assets/images/logo.svg"
+            alt="ArrayCat logo"
+            title="ArrayCat is sweepy after converting so many lists to arrays">
+        </div>
+        <div class="">
+          <h1 class="mb-0">ArrayCat</h1>
+          <h2 class="mb-0">Converts any list to array</h2>
+        </div>
+      </div>
+      <div class="tumbler-wrapper">
+        <div class="tumbler"></div>
+        <img
+          class="sun-light"
+          src="./assets/images/switch_normalmode.svg"
+          alt=""
+        >
+        <img
+          class="moon-light"
+          src="./assets/images/switch_darkmode.svg"
+          alt=""
+        >
+      </div>
+    </div>
+  </nav>
+</header>
+<main class="mx-auto mt-0 px-3 max-w-640">
+  <div>
+    <p class="mt-sm-5 pb-4">
+      ArrayCat is a free tool for converting a list of data into an array. For example, you can convert CSV file into
+      PHP array. Or, into a JavaScript array. Or even into an SQL insertion thingie, which is technically not an array,
+      but cats get confused sometimes.<br><br>
+      The conversion can also transform the data into something better, such as sorting, removing of duplicate elements
+      and converting data to lower case.
+    </p>
+  </div>
+
+  <div class="input-box p-4">
+    <div
+      class="mb-3 d-flex flex-column flex-md-row justify-content-between align-items-center"
+    >
+      <div class="d-flex align-items-center mb-3 mb-md-0">
+        <p class="my-0 mr-2 font-weight-bold">Input data format:</p>
+        <div class="selectBox dropdown">
+          <div
+            class="selectBox__value dropdown-toggle"
+            data-toggle="dropdown"
+            id="inputDataFormat"
+          >
+            One item per row
+          </div>
+          <div class="dropdown-menu">
+            <a href="javascript:void(0)" class="dropdown-item ">Comma separated</a>
+            <a href="javascript:void(0)" class="dropdown-item">Inside quotation marks</a>
+            <a href="javascript:void(0)" class="dropdown-item active">One item per row</a>
+            <a href="javascript:void(0)" class="dropdown-item">Tab separated</a>
+
+          </div>
+        </div>
+      </div>
+      <div class="d-flex">
+        <button class="default-btn mr-2" id="exampleBtn">Example</button>
+        <div class="default-btn" id="uploadBtn">
+          <input type="file" id="uploadfile">
+          Load file
+        </div>
+      </div>
+    </div>
+    <textarea
+      class="text-area w-100"
+      name="inputData"
+      id="inputData"
+      rows="10"
+      spellcheck="false"
+    ></textarea>
+  </div>
+  <div class="text-center my-5">
+    <button class="default-btn" id="convertBtn">Convert</button>
+  </div>
+  <div class="output-box p-4 mb-5">
+    <div
+      class="mb-3 d-flex flex-column flex-md-row justify-content-between align-items-center"
+    >
+      <div class="d-flex align-items-center mb-3 mb-md-0">
+        <p class="my-0 mr-2 font-weight-bold">Output data format:</p>
+        <div class="selectBox dropdown">
+          <div
+            class="selectBox__value dropdown-toggle"
+            data-toggle="dropdown"
+            id="outputDataFormat">
+            JavaScript array
+          </div>
+          <div class="dropdown-menu">
+            <a href="javascript:void(0)" class="dropdown-item">Delphi array</a>
+            <a href="javascript:void(0)" class="dropdown-item active">JavaScript array</a>
+            <a href="javascript:void(0)" class="dropdown-item">One item per row</a>
+            <a href="javascript:void(0)" class="dropdown-item">Pascal array</a>
+            <a href="javascript:void(0)" class="dropdown-item">PHP array</a>
+            <a href="javascript:void(0)" class="dropdown-item">Python array</a>
+            <a href="javascript:void(0)" class="dropdown-item">SQL insert</a>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div class="dropdown mr-2" id="transformDropdown">
+          <button
+            type="button"
+            class="default-btn dropdown-toggle"
+            data-toggle="dropdown">
+            Transform &nbsp;
+          </button>
+          <div class="dropdown-menu px-3">
+            <div class="py-2">
+              <input type="checkbox" id="base64" name="base64">
+              <label class="mb-0" for="base64">Base64 encode elements</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="countDuplicate" name="countDuplicate">
+              <label class="mb-0" for="countDuplicate">Count duplicate elements</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="htmlEntities" name="htmlEntities">
+              <label class="mb-0" for="htmlEntities">Convert to HTML entities</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="lowercase" name="lowercase">
+              <label class="mb-0" for="lowercase">Convert to lower case</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="normalize" name="normalize">
+              <label class="mb-0" for="normalize">Remove diacritics</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="duplicates" name="duplicates">
+              <label class="mb-0" for="duplicates">Remove duplicate elements</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="removeEmptyElement" name="removeEmptyElement" checked>
+              <label class="mb-0" for="removeEmptyElement">Remove empty elements</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="sort" name="sort">
+              <label class="mb-0" for="sort">Sort elements</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="stripWhitespace" name="stripWhitespace">
+              <label class="mb-0" for="stripWhitespace">Strip excess whitespace</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="unifyWhitespace" name="unifyWhitespace">
+              <label class="mb-0" for="unifyWhitespace">Unify whitespace</label>
+            </div>
+            <div class="py-2">
+              <input type="checkbox" id="whitespace" name="whitespace" checked>
+              <label class="mb-0" for="whitespace">Trim whitespace</label>
+            </div>
+
+            <div id="maxrowwidthbox">
+              <label for="maxRowWidth">Maximum row length:</label>
+              <input
+                type="number"
+                id="maxRowWidth"
+                value="512"
+                min="1"
+                name="maxRowWidth"
+              >
+            </div>
+          </div>
+        </div>
+        <button class="default-btn" id="copyBtn">
+          Copy<span class="d-none d-sm-inline"> to clipboard</span>
+        </button>
+      </div>
+    </div>
+    <div class="w-100 bg-white br-12 overflowx-auto output-box-inner">
+          <textarea
+            class="text-area"
+            name="outputData"
+            id="outputData"
+            spellcheck="false"
+            readonly
+          ></textarea>
+    </div>
+  </div>
+  <div class="loading-element-mask">
+    <div class="loading-element">
+      <div class="inner one"></div>
+      <div class="inner two"></div>
+      <div class="inner three"></div>
+    </div>
+  </div>
+</main>
+<footer class="px-3">
+  <div>
+    <p>The following transformations are supported:</p>
+    <ul>
+      <li>Base64 encode each element.</li>
+      <li>Count duplicate elements.</li>
+      <li>Convert to HTML elements. This option will convert all applicable characters to HTML entities. For example,
+        "Durian > Jackfruit" becomes "Durian &amp;gt; Jackfruit".
+      </li>
+      <li>Convert each element to lower case.</li>
+      <li>Remove diacritics. This option will attempt to remove any diacritics and accent characters. For example,
+        converting "Crème Brulée" into "Creme Brulee", which is equally yummy but possibly easier to process.
+      </li>
+      <li>Remove duplicate elements. Removes any elements with identical content, after other transformers have been run
+        first. For example, if your list contains "John" and "john" and you have the <i>Convert each element to lower
+          case</i> enabled, only "john" will be added to the final output.
+      </li>
+      <li>Remove empty elements.</li>
+      <li>Sort elements. If enabled, the final output will be in alphabetical order.</li>
+      <li>Strip excess whitespace. Removes any cases of multiple spaces between words.</li>
+      <li>Unify whitespace. Replaces all whitespace characters with a single space.</li>
+      <li>Trim whitespace. Removes any leading and trailing space characters of each element.</li>
+    </ul>
+
+    <p>
+      <b>Privacy Policy:</b> Cats mind their own business. Data processing happens only inside
+      your browser, no data is sent to anywhere.<br><br>
+      <b>Terms of Service:</b> This service is provided to you “as is” without any guarantees of any kind. If you wish
+      to use this service, you will do so entirely at your own risk. For any feedback, please send an email: <a
+      href="mailto:jouni@arraycat.com">jouni@arraycat.com</a>.<br><br>
+      &copy; Copyright 2023-<?php echo date('Y'); ?> <a href="https://greatsoftwarecompany.com" target="_blank">Great Software Company</a>. All Rights Reserved.
+    </p>
+
+  </div>
+</footer>
+<div class="copy-notification"></div>
+<script src="./assets/js/cookie.js"></script>
+<script src="./assets/js/darkmode.js"></script>
+<script src="./assets/js/jquery.min.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
+<script src="./assets/js/alert.js"></script>
+<script src="./assets/js/script.js"></script>
+</body>
+</html>
